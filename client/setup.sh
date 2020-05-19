08a0102fbf633e5de3d43a01b995e1ca8e68bbd3
Christoffer Kugg Jerkeby <kugghjul@gmail.com>
2020-05-19 13:47:51 +0200

    Fixed a minor env variable bug

--- client/setup.sh
+++ client/setup.sh
@@ -20,9 +20,10 @@ docker volume create --driver local \
 docker build --build-arg URL=$URL --tag $NAME:$VERSION .
 # Set up env variables using --env-file ./variables instead
 for id in `seq 2 $(nproc)`; do
-    docker run -it -v $OUTPUT_VOLUME:/$OUTPUT_VOLUME --name $NAME.$id --env TARGET=$TARGET --env URL=$URL --env JOB="-S $id" --env ID=$id --detach $NAME:$VERSION
+    docker run -it -v $OUTPUT_VOLUME:/$OUTPUT_VOLUME --name $NAME.$id --env-file ./variables --env JOB="-S $id" --env ID=$id --detach $NAME:$VERSION
+    #docker run -it -v $OUTPUT_VOLUME:/$OUTPUT_VOLUME --name $NAME.$id --env TARGET=$TARGET --env URL=$URL --env JOB="-S $id" --env ID=$id --detach $NAME:$VERSION
 done
-docker run -it -v $OUTPUT_VOLUME:/$OUTPUT_VOLUME --name $NAME.1 --env TARGET=$TARGET --env URL=$URL --env JOB="-M master" --env ID=$id --detach $NAME:$VERSION
+docker run -it -v $OUTPUT_VOLUME:/$OUTPUT_VOLUME --name $NAME.1 --env-file ./variables --env JOB="-M master" --env ID=$id --detach $NAME:$VERSION
 
 # Post installation fix for a bug in rsyslog caused by systemd.
 # for id in `seq 1 $(nproc)`; do
